a13bb6c336f71f06cbe084d8ff4e48c22f30adab
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-01-21 15:45:20 +0100

    lz4/huff0 wrappers in liblz4huf

---
+++ src/liblz4huf.c
@@ -0,0 +1,150 @@
+
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "lz4hc.h"
+#include "lz4.h"
+#include "huf.h"
+
+#define LZ4HUF_BS (128 * 1024)
+
+struct buffer {
+    uint8_t error;
+    uint8_t * data;
+    uint32_t size;
+};
+
+// Wrapper functions over compression.
+
+static struct buffer lz4_compress(const uint8_t * src, uint32_t src_size, uint8_t level) {
+    uint32_t dst_capacity = LZ4_compressBound(src_size);
+    uint8_t * dst = malloc(dst_capacity + sizeof(uint32_t));
+    if(dst == NULL) {
+        struct buffer buf;
+        buf.error = 1;
+        buf.data = NULL;
+        buf.size = 0;
+        return buf;
+    }
+
+    // Serialise the original size into the buffer.
+    dst[0] = (src_size >> 24) & 0xFF;
+    dst[1] = (src_size >> 16) & 0xFF;
+    dst[2] = (src_size >> 8) & 0xFF;
+    dst[3] = src_size & 0xFF;
+
+    struct buffer buf;
+    buf.error = 0;
+    buf.data = dst;
+
+    if (level < LZ4HC_CLEVEL_MIN) {
+        buf.size = LZ4_compress_default(src, dst + sizeof(uint32_t), src_size, dst_capacity);
+    } else {
+        buf.size = LZ4_compress_HC(src, dst + sizeof(uint32_t), src_size, dst_capacity, level);
+    }
+    
+    buf.size += sizeof(uint32_t);
+
+    if(buf.size == 0) {
+        buf.error = 1;
+        free(buf.data);
+        buf.data = NULL;
+    }
+
+    return buf;
+}
+
+static struct buffer lz4_decompress(const uint8_t * src, uint32_t src_size) {
+    uint32_t dst_size = (src[0] << 24) | (src[1] << 16) | (src[2] << 8) | src[3];
+
+    uint8_t * dst = malloc(dst_size);
+    if(dst == NULL) {
+        struct buffer buf;
+        buf.error = 1;
+        buf.data = NULL;
+        buf.size = 0;
+        return buf;
+    }
+
+    struct buffer buf;
+    buf.error = 0;
+    buf.data = dst;
+
+    buf.size = LZ4_decompress_safe(src + sizeof(uint32_t), dst, src_size - sizeof(uint32_t), dst_size);
+    if(buf.size == 0) {
+        buf.error = 1;
+        free(buf.data);
+        buf.data = NULL;
+    }
+
+    return buf;
+}
+
+static struct buffer huf_compress(const uint8_t * src, uint32_t src_size) {
+    uint32_t dst_capacity = HUF_compressBound(src_size);
+    uint8_t * dst = malloc(dst_capacity + 1 + sizeof(uint32_t));
+    if(dst == NULL) {
+        struct buffer buf;
+        buf.error = 1;
+        buf.data = NULL;
+        buf.size = 0;
+        return buf;
+    }
+
+    struct buffer buf;
+    buf.error = 0;
+    buf.data = dst;
+    buf.size = dst_capacity;
+
+    buf.size = HUF_compress(dst + 1 + sizeof(uint32_t), dst_capacity, src, src_size);
+    if(buf.size == 0) {
+        // Assume that the data simply can't be compressed...
+        // This requires 
+        memcpy(dst + 1, src, src_size);
+        buf.size = src_size;
+        dst[0] = 0; // Uncompressed
+    } else {
+        dst[0] = 1; // Compressed
+    }
+
+    // Serialise the original size into the buffer.
+    dst[1] = (src_size >> 24) & 0xFF;
+    dst[2] = (src_size >> 16) & 0xFF;
+    dst[3] = (src_size >> 8) & 0xFF;
+    dst[4] = src_size & 0xFF;
+
+    return buf;
+}
+
+static struct buffer huf_decompress(const uint8_t * src, uint32_t src_size) {
+    // Read the compressed flag and the original size.
+    uint8_t compressed = src[0];
+    uint32_t dst_size = (src[1] << 24) | (src[2] << 16) | (src[3] << 8) | src[4];
+
+    uint8_t * dst = malloc(dst_size);
+    if(dst == NULL) {
+        struct buffer buf;
+        buf.error = 1;
+        buf.data = NULL;
+        buf.size = 0;
+        return buf;
+    }
+
+    struct buffer buf;
+    buf.error = 0;
+    buf.data = dst;
+    buf.size = dst_size;
+
+    if(compressed) {
+        buf.size = HUF_decompress(dst, dst_size, src + 1 + sizeof(uint32_t), src_size - (1 + sizeof(uint32_t)));
+        if(buf.size == 0) {
+            buf.error = 1;
+            free(buf.data);
+            buf.data = NULL;
+        }
+    } else {
+        memcpy(dst, src + 1, dst_size);
+    }
+
+    return buf;
+}
